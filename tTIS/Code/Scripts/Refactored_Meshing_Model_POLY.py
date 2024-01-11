47f51bebb36538e53ee7731c866097de05077202
DFrogBinder <boyan.ivanov32@abv.bg>
2024-01-11 17:14:38 +0200

    a little more refactoring of the meshing script to ommit stls that I do not currently have

--- tTIS/Code/Scripts/Refactored_Meshing_Model_POLY.py
+++ tTIS/Code/Scripts/Refactored_Meshing_Model_POLY.py
@@ -26,6 +26,8 @@ def main_function(settings_file, stl_models_path, electrode_attributes_path,save
     for folder in folders:
         if folder == 'meshed':
             continue
+        elif folder == 'Raw':
+            continue
         print("############")
         print("Model " + folder)
         print("############\n")
@@ -41,12 +43,22 @@ def main_function(settings_file, stl_models_path, electrode_attributes_path,save
 
         skin_stl = pymesh.load_mesh(os.path.join(stl_models_path, folder, 'skin_fixed.stl'))
 
+        geometry_list = [
+                        'skin_fixed.stl', 
+                         'skull_fixed.stl', 
+                         'csf_fixed.stl', 
+                        # 'gm_fixed.stl', 
+                        # 'wm_fixed.stl', 
+                        # 'cerebellum_fixed.stl', 
+                        # 'ventricles_fixed.stl'
+                         ]
+        
         meshing = MeshOps.MeshOperations(skin_stl, elec_attributes)
-        meshing.load_surface_meshes(os.path.join(stl_models_path, folder), ['skin_fixed.stl', 'skull_fixed.stl', 'csf_fixed.stl', 'gm_fixed.stl', 'wm_fixed.stl', 'cerebellum_fixed.stl', 'ventricles_fixed.stl'])
+        meshing.load_surface_meshes(os.path.join(stl_models_path, folder), geometry_list)
         meshing.phm_model_meshing(os.path.join(save_dir, 'meshed_model_10-10_' + folder + '.poly'), electrodes_to_omit=electrodes_to_omit)
 
     
-stl_models_path = '/home/cogitatorprime/sandbox/TI_Pipeline/tTIS/Sphere_CAD'
+stl_models_path = '/home/cogitatorprime/sandbox/TI_Pipeline/tTIS/Sphere_CAD/'
 setting_yml_path = '/home/cogitatorprime/sandbox/TI_Pipeline/tTIS/Code/sim_settings.yml'
 el_attributes_dir = '/home/cogitatorprime/sandbox/TI_Pipeline/mesh2eeg'
 save_dir = '/home/cogitatorprime/sandbox/TI_Pipeline'

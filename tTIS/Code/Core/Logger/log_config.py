e10d1aeece611ac07a49c6e51047a147c9020a62
DFrogBinder <boyan.ivanov32@abv.bg>
2024-02-21 11:52:53 +0000

    Initial commit for a logger module to help with debuging

---
+++ tTIS/Code/Core/Logger/log_config.py
@@ -0,0 +1,19 @@
+import logging
+'''
+In this configuration, all messages of level DEBUG and above are logged to a file named app.log, 
+while messages of level INFO and above are also printed to the console. 
+Adjust the level and format as needed for your application.
+''''
+def setup_logging():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        filename='app.log',
+                        filemode='a')
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+

c7a40c1e50e3f2cc4b2887239a6df946be89a029
DFrogBinder <boyan.ivanov32@abv.bg>
2024-04-18 14:47:18 +0300

    re-organization for files + add new visualizations for standard TI

--- Custom_Python_FEM/Visualization_Scripts/visualize_vtk.py
+++ Custom_Python_FEM/Visualization_Scripts/visualize_vtk.py
@@ -10,35 +10,35 @@ def symlog(data, linthresh=1):
     return result
 
 def Generate_Plot(field,region_mesh,array_type,base_electrode_mesh,df_electrode_mesh):
-    data_array = getattr(region_mesh,array_type)
+    # data_array = getattr(region_mesh,array_type)
     
-    # Determine the shift needed to make the smallest value positive (if min_value is negative)
-    shift = int(abs(np.min(data_array[field]))) + 1   # Adding 1 to ensure the minimum shifted value is positive
+    # # # Determine the shift needed to make the smallest value positive (if min_value is negative)
+    # # shift = int(abs(np.min(data_array[field]))) + 1   # Adding 1 to ensure the minimum shifted value is positive
     
-    # Apply the shift-and-log transformation
-    shifted_and_logged_data = np.log10(data_array[field] + shift)
+    # # # Apply the shift-and-log transformation
+    # # shifted_and_logged_data = np.log10(data_array[field] + shift)
     
-    # Apply symmetric log transformation to the scalar values
-    linthresh = 0.1  # Threshold for symlog transformation, adjust based on your data
-    shifted_and_logged_data = symlog(data_array[field], linthresh)
+    # # # Apply symmetric log transformation to the scalar values
+    # # linthresh = 0.1  # Threshold for symlog transformation, adjust based on your data
+    # # shifted_and_logged_data = symlog(data_array[field], linthresh)
     
-    # Update the mesh with the transformed data
-    if 'point' in array_type:
-        region_mesh.point_arrays[field] = shifted_and_logged_data
-    elif "cell" in array_type:
-        region_mesh.cell_arrays[field] = shifted_and_logged_data
+    # # Update the mesh with the transformed data
+    # if 'point' in array_type:
+    #     region_mesh.point_arrays[field] = shifted_and_logged_data
+    # elif "cell" in array_type:
+    #     region_mesh.cell_arrays[field] = shifted_and_logged_data
     
     # Create a new Plotter instance for each field
-    SubPlot_Number = 4
-    plotter = pv.Plotter(shape=(int(SubPlot_Number/2), int(SubPlot_Number/2)))
+    SubPlot_Number = 1
+    plotter = pv.Plotter(shape=(int(SubPlot_Number), int(SubPlot_Number)))
     plotter.set_background('black')  # Changing background to black
 
     for i in range(SubPlot_Number):
-        r, c = divmod(i, int(SubPlot_Number/2))  # Calculate row and column index
+        r, c = divmod(i, int(SubPlot_Number))  # Calculate row and column index
         plotter.subplot(r, c)
         colormap_index = i % len(colormaps)  # Ensure we loop through the colormaps
         plotter.add_mesh(region_mesh, scalars=field, cmap=colormaps[colormap_index], opacity=1)
-        plotter.add_scalar_bar(title=f'SymLog {field}', title_font_size=22, label_font_size=20, n_labels=3, shadow=True, font_family='arial')
+        # plotter.add_scalar_bar(title=f'SymLog {field}', title_font_size=22, label_font_size=20, n_labels=3, shadow=True, font_family='arial')
 
         # Determine the correct electrode mesh to use
         # TODO: Check if 'not' keyword is correct here
@@ -49,7 +49,7 @@ def Generate_Plot(field,region_mesh,array_type,base_electrode_mesh,df_electrode_
     plotter.show()
     
 # Load the .vtk file and slice it
-mesh = pv.read('/home/cogitatorprime/sandbox/TI_Pipeline/tTIS/Simple_Export_Save_Dir/fem_model-name_12-13-10-11.vtk').slice(normal=[0, 0, 1], origin=[0, 0, 0])
+mesh = pv.read('/home/cogitatorprime/sandbox/TI_Pipeline/Custom_Python_FEM/tTIS/Simple_Export_Save_Dir/fem_model-name_12-13-10-11.vtk').slice(normal=[0, 0, 1], origin=[0, 0, 0])
 
 if 'mat_id' in mesh.cell_arrays:
     brain_mask = mesh.cell_arrays['mat_id'] == 4  # 1-4 tissue types | 10-13 electrodes
@@ -63,7 +63,7 @@ if 'mat_id' in mesh.cell_arrays:
     df_electrode_mesh = mesh.extract_cells(df_electrodes_mask)
 
     fields = ['potential_base', 'potential_df']
-    colormaps = ['viridis', 'plasma', 'inferno', 'coolwarm']
+    colormaps = ['coolwarm']
     
     for i, field in enumerate(fields):
         if field in region_mesh.cell_arrays:

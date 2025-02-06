01ec2fcefd9a16744e08e399e36dbeb999ad110e
boyan <boyan.ivanov32@abv.bg>
2025-02-06 14:05:19 +0000

    initial commit for gmsh-based mesh manipulation

---
+++ Optimization/camcan-preprocess/gmsh_inspect.py
@@ -0,0 +1,66 @@
+import gmsh
+import numpy as np
+
+
+def get_physical_groups():
+    physical_groups = gmsh.model.getPhysicalGroups()
+    
+    return physical_groups
+
+def get_model_stats(mesh):
+    
+    # Get mesh nodes and elements count
+    node_count = len(mesh.getNodes()[0])
+    element_count = len(mesh.getElements()[1][0])
+    print("Mesh Statistics:")
+    print(f"Number of nodes: {node_count}")
+    print(f"Number of elements: {element_count}")
+   
+def load_msh_file(file_path):
+    gmsh.initialize()
+    gmsh.open(file_path)
+
+    # Get information about the mesh
+    model = gmsh.model
+    mesh = model.mesh
+    
+    get_model_stats(mesh)
+    initial_physical_groups = get_physical_groups()
+    print("Initial physical groups number:", len(initial_physical_groups))
+
+    # physical_groups = gmsh.model.getPhysicalGroups()
+
+
+    # Get nodes and elements
+    nodes = mesh.getNodes()
+    elements = mesh.getElements()
+       
+    defined_elements_types = gmsh.model.mesh.getElementTypes()
+    print(f'{defined_elements_types}')
+
+    # Choose which physical group to remove (e.g., if tag is 1 and dimension is 2 for surfaces)
+    dimension_to_remove = 2  # 0: Points, 1: Edges, 2: Surfaces, 3: Volumes
+    tag_to_remove = [11,12]  # Replace with the actual tag of the region you want to remove
+
+    for tag in tag_to_remove:
+        
+        entities_to_remove = []
+        entity_tuples = gmsh.model.getEntitiesForPhysicalGroup(dimension_to_remove, tag)
+        for entity in entity_tuples:
+            entities_to_remove.append(entity)  # entity is a tuple (dimension, tag)
+
+        
+        # Remove the unwanted physical group
+        gmsh.model.removePhysicalGroups([(dimension_to_remove, entities_to_remove[0])])
+        
+    gmsh.write("modified_model.msh")
+    print("Modified model saved")
+    
+    get_model_stats(mesh)
+    final_physical_groups = get_physical_groups()
+    print("Final physical groups number:", len(final_physical_groups))
+    
+    gmsh.finalize()
+
+# Example usage
+load_msh_file("head.msh")
